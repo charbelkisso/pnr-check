8fb0c4a55222a6f2fc5d5cafab6435d40ac17270
Kisso <charbelkisso@gmail.com>
2021-03-24 13:03:53 +0100

    formating

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import * as express from 'express';
-import { config } from 'dotenv';
-import PersonalNumber from './PersonalNumber';
+import * as express from "express";
+import { config } from "dotenv";
+import PersonalNumber from "./PersonalNumber";
 
 const app = express();
 const port = process.env.PORT || 8080;
 config();
 
-app.get('/pnrcheck/:pnr', (req: express.Request, res: express.Response) => {
-    let pnr = new PersonalNumber(req.params['pnr']);
+app.get("/pnrcheck/:pnr", (req: express.Request, res: express.Response) => {
+    let pnr = new PersonalNumber(req.params["pnr"]);
     let responseMessage = pnr.checkPersonalNumber();
 
     if (!responseMessage && responseMessage === null)
         res.status(500).json({
-            error: "Internal Error!"
+            error: "Internal Error!",
         });
-    else
-        res.status(200).json(responseMessage)
+    else res.status(200).json(responseMessage);
 });
 
-
-app.listen(port, () => { console.log("service is running on port: ", port) });
+app.listen(port, () => {
+    console.log("service is running on port: ", port);
+});
